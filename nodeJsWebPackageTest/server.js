ad13ed7e25610a4b6410b205b1574321bf4b6947
Aarsh Talati <aarshstalati1989@gmail.com>
2017-03-13 23:34:25 -0400

    configured application

--- nodeJsWebPackageTest/server.js
+++ nodeJsWebPackageTest/server.js
@@ -1,16 +1,43 @@
 'use strict';
 var http = require('http');
+
 var express = require('express');
-var app = express();
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var port = process.env.port || 1337;
+var app = express();
 
-// view engine
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
 app.set("view engine", "jade");
 
-app.get("/", function (req, res) {
-    res.render("jade/index", { title: "Demo App 123" });
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+}
+));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('partials/:partialPath', function (req, res) {
+    res.render('partials/' + req.param.partialPath);
 });
 
+app.get('*', function (req, res) {
+    res.render('index');
+});
+
+
 var server = http.createServer(app);
-server.listen(port);
+server.listen(port, function () {
+    console.log('My app is listening at: ', port);
+});
